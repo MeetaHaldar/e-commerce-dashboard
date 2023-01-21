8d9c1df34d5a645a010d45215baa3d896e60914e
meeta haldar <meetahaldar1001@gmail.com>
2023-01-21 15:26:52 +0530

    created profile component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Login from "./components/Login"
 import AddProduct from './components/AddProduct'
 import ProductList from './components/ProductList'
 import UpdateProduct from './components/UpdateProduct'
+import Profile from './components/Profile'
 function App() {
   return (
     <div className="App">
@@ -19,7 +20,8 @@ function App() {
         <Route element ={<PrivateComponent/>}>
         <Route path="/"element={<ProductList/>}/>
         <Route path="/add"element={<AddProduct/>} />
-        <Route path="/profile"element={<h1>your profile</h1>} />
+        <Route path="/profile"element={<Profile/>} />
+        <Route path="/update"element={<h2>Nothing here to update</h2>} />
         <Route path="/update/:id"element={<UpdateProduct/>} />
         <Route path="/logout"element={<h1>logout</h1>} />
         </Route>
---
+++ client/src/components/Profile.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+export default  function Profile(){
+    const auth = localStorage.getItem('User')
+    return (
+<div><Card className="text-center">
+      <Card.Header>Profile Card</Card.Header>
+      <Card.Body>
+        <Card.Text> Name :  {JSON.parse(auth).name}</Card.Text>
+        <Card.Text> Email :  {JSON.parse(auth).email}
+        </Card.Text>
+      </Card.Body>
+    </Card></div>
+    );
+
+}
+
