9653090251fa4521f6154a1d7a9d51178ce58182
meeta haldar <meetahaldar1001@gmail.com>
2023-02-04 17:46:29 +0530

    changed model

--- server/model/Product.js
+++ server/model/Product.js
@@ -1,21 +1,31 @@
 const mongoose = require("mongoose");
-
 const productSchema = mongoose.Schema({
   name: {
     type: String,
+    required: true,
+    index: true,
   },
   price: {
     type: Number,
+    required: true,
+    index: true,
   },
   category: {
     type: String,
+    required: true,
+    index: true,
   },
 
   company: {
     type: String,
+    required: true,
+    index: true,
   },
   userId: {
     type: String,
+    required: true,
+    index: true,
+    ref: "User",
   },
 });
 
